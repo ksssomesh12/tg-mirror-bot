770c3d55d8a30db65b167a7d1e7e1afc725a87fc
deadsec45 <79352535+deadsec45@users.noreply.github.com>
2021-04-10 13:22:03 +0530

    Update bot_utils.py

--- tgmb/helper/ext_utils/bot_utils.py
+++ tgmb/helper/ext_utils/bot_utils.py
@@ -24,7 +24,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['●', '●', '●', '●', '●', '●', '●']
+PROGRESS_INCOMPLETE = ['⎊', '⎊', '⎊', '⎊', '⎊', '⎊', '⎊']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -80,7 +80,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '●' * cFull
+    p_str = '⎊' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
     p_str += '○' * (PROGRESS_MAX_SIZE - cFull)
